8601e4620520cdf7d4a0469fa228bedcb2c9987e
Wallys <wallysbouchama08@gmail.com>
2025-11-21 09:42:00 +0100

    commit1

--- parking.py
+++ parking.py
@@ -2,7 +2,89 @@ class Parking:
 
     def __init__(self):
         self.niv = 1
-        self.place = 0
+        self.place_niv1 = []
+        self.place_niv2 = []
+        self.place_niv3 = []
+        self.place_niv4 = []
+        self.place_niv5 = []
+
+    def place(self):
+        for i in range(80):
+            self.place_niv1.append(False)
+            self.place_niv2.append(False)
+            self.place_niv3.append(False)
+            self.place_niv4.append(False)
+            self.place_niv5.append(False)
+
+    def si_voiture(self, niv, place):
+        if niv == 1:
+            for i in range(len(self.place_niv1)):
+                if i + 1 == place:
+                    if self.place_niv1[i] == True:
+                        print("place non libre")
+                        return False
+                    else:
+                        print("place libre")
+                        return True
+        if niv == 2:
+            for i in range(len(self.place_niv2)):
+                if i + 1 == place:
+                    if self.place_niv2[i] == True:
+                        print("place non libre")
+                        return False
+                    else:
+                        print("place libre")
+                        return True
+        if niv == 3:
+            for i in range(len(self.place_niv3)):
+                if i + 1 == place:
+                    if self.place_niv3[i] == True:
+                        print("place non libre")
+                        return False
+                    else:
+                        print("place libre")
+                        return True
+        if niv == 4:
+            for i in range(len(self.place_niv4)):
+                if i + 1 == place:
+                    if self.place_niv4[i] == True:
+                        print("place non libre")
+                        return False
+                    else:
+                        print("place libre")
+                        return True
+        if niv == 5:
+            for i in range(len(self.place_niv5)):
+                if i + 1 == place:
+                    if self.place_niv5[i] == True:
+                        print("place non libre")
+                        return False
+                    else:
+                        print("place libre")
+                        return True
+        
+    def ajout_place(self, niv, place):
+        if parking.si_voiture(niv, place) == True:
+            print("vous avez pris la place")
+            if niv == 1:
+                self.place_niv1[place-1] = True
+            if niv == 2:
+                self.place_niv2[place-1] = True
+            if niv == 3:
+                self.place_niv3[place-1] = True
+            if niv == 4:
+                self.place_niv4[place-1] = True
+            if niv == 5:
+                self.place_niv5[place-1] = True
+        else:
+            print("vous pouvez pas prendre la place")
+
+    def __str__(self):
+        print(f"{self.place_niv1}"+
+              f"{self.place_niv2}"+
+              f"{self.place_niv3}"+
+              f"{self.place_niv4}"+
+              f"{self.place_niv5}")
 
 class Voiture:
 
@@ -20,3 +102,6 @@ class Voiture:
 
 voiture1 = Voiture(12, "BMW", "Jean", True)
 voiture1.information()
+parking = Parking()
+parking.place()
+parking.ajout_place(5, 80)
