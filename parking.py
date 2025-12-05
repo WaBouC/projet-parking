51262b33f8ed15784bb6d867c3058f62451abd03
Wallys <wallysbouchama08@gmail.com>
2025-12-05 08:21:49 +0100

    commit1

--- parking.py
+++ parking.py
@@ -15,6 +15,18 @@ class Parking:
             self.place_niv3.append(False)
             self.place_niv4.append(False)
             self.place_niv5.append(False)
+    
+    def abonnement(self, voiture):
+        if voiture.ab == False:
+            a = str(input("voulez-vous vous abonnez : "))
+            if a == "oui" or a == "Oui":
+                voiture.ab = True
+        else:
+            print("vous êtes déjà abonné")
+            if voiture.ab == True:
+                a = str(input("voulez-vous vous désabonner ? : "))
+                if a == "oui" or a == "Oui":
+                    voiture.ab = False
 
     def si_voiture(self, niv, place):
         if niv == 1:
@@ -63,7 +75,7 @@ class Parking:
                         print("place libre")
                         return True
         
-    def ajout_place(self, niv, place):
+    def ajout_place(self, niv, place, voiture):
         if parking.si_voiture(niv, place) == True:
             print("vous avez pris la place")
             if niv == 1:
@@ -100,8 +112,10 @@ class Voiture:
                f"(proprietaire : {self.pr})",
                f"(abonnement : {self.ab})")
 
-voiture1 = Voiture(12, "BMW", "Jean", True)
+voiture1 = Voiture(12, "BMW", "Jean", False)
 voiture1.information()
 parking = Parking()
 parking.place()
-parking.ajout_place(5, 80)
+parking.ajout_place(5, 80, voiture1)
+parking.abonnement(voiture1)
+voiture1.information()
